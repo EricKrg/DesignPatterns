29d17b1be6ac132683bdf59a5dfc72fcab5966d5
EricKrg <eric.krueger@uni-jena.de>
2019-01-07 14:25:22 +0100

    init DesingPatterns

---
+++ src/main/java/com/eric/Command/CommandTestrunner.java
@@ -0,0 +1,7 @@
+package com.eric.Command;
+
+public class CommandTestrunner {
+    public static void main(String args[]){
+        // run the pattern
+    }
+}
---
+++ src/main/java/com/eric/Strategy/StrategyTestrunner.java
@@ -0,0 +1,7 @@
+package com.eric.Strategy;
+
+public class StrategyTestrunner {
+    public static void main(String args[]){
+        // run the pattern
+    }
+}
